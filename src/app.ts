706b2c409557c4e4e5aa314a8e87543511a9afe8
Tim aka tynroar <tynrare@gmail.com>
2024-01-04 21:01:01 +0300

    amind fix

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ class App {
         const dt = (now - this.timestamp);
         const dtscaled = dt / 1000;
         this.timestamp = now;
-        const deltaref = Math.max(dt / this.REF_DELTATIME, 3);
+        const deltaref = Math.min(dt / this.REF_DELTATIME, 3);
 
         this.scene_game.step(dtscaled, deltaref);
         this.scene_render.step(dtscaled, deltaref);
